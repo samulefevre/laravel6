e472b2c6c1c7a3cecbe6492159e6607e5a9d1b1a
samuel.lefevr@gmail.com <samu@mac-mini.home>
2019-09-12 21:02:27 +0200

    add tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
         <!-- Styles -->
         <style>
             html, body {
