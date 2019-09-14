e62e7c95ad2194b39193ea5cf889f821984520bd
samuel.lefevr@gmail.com <samu@mini-de-samuel.home>
2019-09-14 07:45:25 +0200

    configs for google app engine

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,4 +52,17 @@ $app->singleton(
 |
 */
 
+$app->useStoragePath(env('APP_STORAGE', base_path() . '/storage'));
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+|
+| This script returns the application instance. The instance is given to
+| the calling script so we can separate the building of the instances
+| from the actual running of the application and sending responses.
+|
+*/
+
 return $app;
